2b71499c68e102ea0edf78955040c0fecedf6f0a
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-04-23 21:48:30 +0200

    View for adding review (new.html.erb) added, new and create method implemented, creation of review in db works

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @review = Review.new
+  end
+  def create
+    @review = Review.new(get_params)
+    @list = List.find(params[:list_id])
+    @review.list = @list
+    if @review.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+end
+
+  private
+  def get_params
+    params.require(:review).permit(:content)
+  end
 end
